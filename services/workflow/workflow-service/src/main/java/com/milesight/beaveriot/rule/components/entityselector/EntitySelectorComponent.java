81dc4e10077e82046e32f8c01aa9822cf10d8307
loong <yusl@milesight.com>
2024-12-18 16:52:36 +0800

    feat: add flow node component

---
+++ services/workflow/workflow-service/src/main/java/com/milesight/beaveriot/rule/components/entityselector/EntitySelectorComponent.java
@@ -0,0 +1,44 @@
+package com.milesight.beaveriot.rule.components.entityselector;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.milesight.beaveriot.base.utils.JsonUtils;
+import com.milesight.beaveriot.context.api.EntityValueServiceProvider;
+import com.milesight.beaveriot.context.integration.model.ExchangePayload;
+import com.milesight.beaveriot.rule.annotations.OutputArguments;
+import com.milesight.beaveriot.rule.annotations.RuleNode;
+import com.milesight.beaveriot.rule.api.ProcessorNode;
+import com.milesight.beaveriot.rule.constants.RuleNodeType;
+import com.milesight.beaveriot.rule.support.SpELExpressionHelper;
+import lombok.Data;
+import org.apache.camel.Exchange;
+import org.apache.camel.spi.UriParam;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author loong
+ * @date 2024/12/18 8:47
+ */
+@RuleNode(value = "entitySelector", type = RuleNodeType.ACTION, description = "EntitySelector")
+@Data
+public class EntitySelectorComponent implements ProcessorNode<Exchange> {
+
+    @OutputArguments
+    @UriParam(javaType = "java.util.List")
+    private List<String> entities;
+
+    @Autowired
+    EntityValueServiceProvider entityValueServiceProvider;
+
+    @Override
+    public void processor(Exchange exchange) {
+        List<String> entitiesVariables = (List<String>) SpELExpressionHelper.resolveExpression(exchange, entities);
+        Map<String, JsonNode> entityValues = entityValueServiceProvider.findValuesByKeys(entitiesVariables);
+        Map<String, Object> entityValuesMap = entityValues.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, entry -> JsonUtils.cast(entry.getValue(), Object.class)));
+        ExchangePayload exchangePayload = ExchangePayload.create(entityValuesMap);
+        exchange.getIn().setBody(exchangePayload);
+    }
+}
---
+++ services/workflow/workflow-service/src/main/java/com/milesight/beaveriot/rule/components/serviceinvocation/ServiceInvocationComponent.java
@@ -0,0 +1,42 @@
+package com.milesight.beaveriot.rule.components.serviceinvocation;
+
+import com.milesight.beaveriot.context.api.EntityValueServiceProvider;
+import com.milesight.beaveriot.context.api.ExchangeFlowExecutor;
+import com.milesight.beaveriot.context.integration.model.ExchangePayload;
+import com.milesight.beaveriot.rule.annotations.RuleNode;
+import com.milesight.beaveriot.rule.api.ProcessorNode;
+import com.milesight.beaveriot.rule.constants.RuleNodeType;
+import com.milesight.beaveriot.rule.support.SpELExpressionHelper;
+import lombok.Data;
+import org.apache.camel.Exchange;
+import org.apache.camel.spi.UriParam;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
+
+/**
+ * @author loong
+ * @date 2024/12/23 11:34
+ */
+@RuleNode(value = "serviceInvocation", type = RuleNodeType.ACTION, description = "Service Invocation")
+@Data
+public class ServiceInvocationComponent implements ProcessorNode<Exchange> {
+
+    @UriParam(javaType = "java.util.Map", prefix = "bean")
+    private Map<String, Object> serviceParams;
+
+    @Autowired
+    EntityValueServiceProvider entityValueServiceProvider;
+    @Autowired
+    private ExchangeFlowExecutor exchangeFlowExecutor;
+
+    @Override
+    public void processor(Exchange exchange) {
+        Map<String, Object> exchangePayloadVariables = SpELExpressionHelper.resolveExpression(exchange, serviceParams);
+        ExchangePayload payload = ExchangePayload.create(exchangePayloadVariables);
+
+        exchange.getIn().setBody(payload);
+
+        exchangeFlowExecutor.syncExchangeDown(payload);
+    }
+}
