65759e17f83939dbd61ddb34d1161bc58e82a371
loong <yusl@milesight.com>
2024-12-25 17:30:38 +0800

    fix: Modify serviceInvocation parameter parsing

--- services/workflow/workflow-service/src/main/java/com/milesight/beaveriot/rule/components/serviceinvocation/ServiceInvocationComponent.java
+++ services/workflow/workflow-service/src/main/java/com/milesight/beaveriot/rule/components/serviceinvocation/ServiceInvocationComponent.java
@@ -8,6 +8,7 @@ import com.milesight.beaveriot.rule.annotations.RuleNode;
 import com.milesight.beaveriot.rule.annotations.UriParamExtension;
 import com.milesight.beaveriot.rule.api.ProcessorNode;
 import com.milesight.beaveriot.rule.constants.RuleNodeType;
+import com.milesight.beaveriot.rule.support.JsonHelper;
 import com.milesight.beaveriot.rule.support.SpELExpressionHelper;
 import lombok.Data;
 import org.apache.camel.Exchange;
@@ -26,7 +27,7 @@ public class ServiceInvocationComponent implements ProcessorNode<Exchange> {
 
     @UriParam(javaType = "java.util.Map", prefix = "bean")
     @UriParamExtension(uiComponent = "serviceEntitySetting")
-    private Map<String, Object> serviceParams;
+    private Map<String, Object> serviceInvocationSetting;
 
     @Autowired
     EntityValueServiceProvider entityValueServiceProvider;
@@ -35,6 +36,7 @@ public class ServiceInvocationComponent implements ProcessorNode<Exchange> {
 
     @Override
     public void processor(Exchange exchange) {
+        Map<String,Object> serviceParams = JsonHelper.fromJSON(serviceInvocationSetting.get("serviceParams").toString(), Map.class);
         Map<String, Object> exchangePayloadVariables = SpELExpressionHelper.resolveExpression(exchange, serviceParams);
         ExchangePayload payload = ExchangePayload.create(exchangePayloadVariables);
 
