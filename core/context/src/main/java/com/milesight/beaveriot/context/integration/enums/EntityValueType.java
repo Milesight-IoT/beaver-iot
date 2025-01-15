e4e0cce841735d01f88f78bec1c0720c92c71212
leon <408970922@qq.com>
2025-01-15 10:15:51 +0800

    refactor: Compatible with converted entity value types

--- core/context/src/main/java/com/milesight/beaveriot/context/integration/enums/EntityValueType.java
+++ core/context/src/main/java/com/milesight/beaveriot/context/integration/enums/EntityValueType.java
@@ -1,8 +1,17 @@
 package com.milesight.beaveriot.context.integration.enums;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.milesight.beaveriot.base.enums.ErrorCode;
+import com.milesight.beaveriot.base.exception.ServiceException;
+import com.milesight.beaveriot.base.utils.JsonUtils;
 import com.milesight.beaveriot.context.integration.entity.annotation.Entities;
+import com.milesight.beaveriot.context.integration.model.Entity;
+import com.milesight.beaveriot.context.integration.model.ExchangePayload;
+import org.springframework.util.ObjectUtils;
 
 import java.math.BigDecimal;
+import java.util.Base64;
+import java.util.Map;
 
 /**
  * @author leon
@@ -29,7 +38,61 @@ public enum EntityValueType {
         }
     }
 
+    public <T> Object convertValue(T value) {
+        if (value == null) {
+            return null;
+        }
+        switch (this) {
+            case STRING:
+                return value instanceof String ? value : JsonUtils.cast(value, String.class);
+            case LONG:
+                return value instanceof Long ? value : JsonUtils.cast(value, Long.class);
+            case DOUBLE:
+                return value instanceof Double ? value : JsonUtils.cast(value, Double.class);
+            case BOOLEAN:
+                return value instanceof Boolean ? value : JsonUtils.cast(value, Boolean.class);
+            case BINARY:
+                if (value instanceof byte[] bytes) {
+                    return bytes;
+                } else {
+                    return Base64.getDecoder().decode(String.valueOf(value));
+                }
+            case OBJECT:
+                return value;
+            default:
+                throw ServiceException.with(ErrorCode.PARAMETER_VALIDATION_FAILED).build();
+        }
+    }
+
+    public static ExchangePayload convertValue(ExchangePayload exchangePayload) {
+        if (ObjectUtils.isEmpty(exchangePayload)) {
+            return exchangePayload;
+        }
+        Map<String, Entity> exchangeEntities = exchangePayload.getExchangeEntities();
+        exchangePayload.forEach((key, value) -> {
+            if (exchangeEntities.containsKey(key)) {
+                Entity entity = exchangeEntities.get(key);
+                exchangePayload.put(key, entity.getValueType().convertValue(value));
+            }
+        });
+        return exchangePayload;
+    }
+
     public static boolean hasEntitiesAnnotation(Class<?> clazz) {
         return clazz.isAnnotationPresent(Entities.class);
+    }
+
+    public static void main(String[] args) {
+        String json = "{\"name\":\"leon\",\"age\":18.1}";
+        JsonNode jsonNode = JsonUtils.fromJSON(json);
+        JsonNode age = jsonNode.get("age");
+
+        Object o = EntityValueType.of(Double.class).convertValue(age);
+        System.out.println(EntityValueType.of(String.class));
+        System.out.println(EntityValueType.of(Integer.class));
+        System.out.println(EntityValueType.of(Float.class));
+        System.out.println(EntityValueType.of(Boolean.class));
+        System.out.println(EntityValueType.of(byte[].class));
+        System.out.println(EntityValueType.of(Object.class));
     }
 }
