7aeba129d002b7b87379e43526a0ff0e1a94d3b4
Maglitch65 <majiajun@milesight.com>
2025-01-18 16:52:45 +0800

    feat(rule-engine): SimpleTimer supports interval-based looping

--- core/rule-engine/rule-engine-components/src/main/java/com/milesight/beaveriot/rule/components/timer/SimpleTimerConsumer.java
+++ core/rule-engine/rule-engine-components/src/main/java/com/milesight/beaveriot/rule/components/timer/SimpleTimerConsumer.java
@@ -5,13 +5,13 @@ import lombok.extern.slf4j.*;
 import org.apache.camel.Endpoint;
 import org.apache.camel.Processor;
 import org.apache.camel.support.DefaultConsumer;
-import org.springframework.util.Assert;
 
 import java.time.ZoneId;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 
 @Slf4j
@@ -27,26 +27,38 @@ public class SimpleTimerConsumer extends DefaultConsumer {
         Objects.requireNonNull(settings, "timer settings is null");
         Objects.requireNonNull(settings.getType(), "timer settings type is null");
 
-        if (SimpleTimerSettings.TimerType.ONCE.equals(settings.getType())) {
-            return getRule(settings.getTimezone(), settings.getExecutionEpochSecond());
-        } else {
-            return getRule(settings.getTimezone(), settings.getRules(), settings.getExpirationEpochSecond());
-        }
+        return switch (settings.getType()) {
+            case ONCE -> getRule(settings.getTimezone(), settings.getExecutionEpochSecond());
+            case INTERVAL -> getRule(settings.getTimezone(), settings.getIntervalTimeUnit(), settings.getIntervalTime());
+            default -> getRule(settings.getTimezone(), settings.getRules(), settings.getExpirationEpochSecond());
+        };
     }
 
     private static SimpleTimerRule getRule(String timezone, Long executionEpochSecond) {
-        return getRule(timezone, null, Collections.singletonList(executionEpochSecond), null);
+        return getRule(timezone, null, Collections.singletonList(executionEpochSecond), null, null);
+    }
+
+    private static SimpleTimerRule getRule(String timezone, TimeUnit intervalTimeUnit, Long intervalTime) {
+        if (intervalTimeUnit == null || intervalTimeUnit.ordinal() < TimeUnit.MINUTES.ordinal()) {
+            throw new IllegalArgumentException("invalid intervalTimeUnit");
+        }
+        if (intervalTime == null || intervalTime < 1) {
+            throw new IllegalArgumentException("invalid intervalTime");
+        }
+        var intervalSeconds = intervalTimeUnit.toSeconds(intervalTime);
+        return getRule(timezone, null, null, Collections.singletonList(intervalSeconds), null);
     }
 
     private static SimpleTimerRule getRule(String timezone,
                                            List<SimpleTimerRuleSettings> rules,
                                            Long expirationEpochSecond) {
-        return getRule(timezone, rules, null, expirationEpochSecond);
+        return getRule(timezone, rules, null, null, expirationEpochSecond);
     }
 
     private static SimpleTimerRule getRule(String timezone,
                                            List<SimpleTimerRuleSettings> rules,
                                            List<Long> externalExecutionEpochSeconds,
+                                           List<Long> intervalSeconds,
                                            Long expirationEpochSecond) {
         var zoneId = Optional.ofNullable(timezone)
                 .map(ZoneId::of)
@@ -58,7 +70,7 @@ public class SimpleTimerConsumer extends DefaultConsumer {
                 .map(r -> ExecutionTime.forCron(r.toCron()))
                 .toList()
                 : null;
-        return new SimpleTimerRule(tz, executionTimes, externalExecutionEpochSeconds, expirationEpochSecond);
+        return new SimpleTimerRule(tz, executionTimes, externalExecutionEpochSeconds, intervalSeconds, expirationEpochSecond);
     }
 
     public String getId() {
