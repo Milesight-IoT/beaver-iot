a9b9a0cdb516d907b111eb76c2aa41c476bcd878
loong-y <yusl@milesight.com>
2025-03-21 18:03:41 +0800

    feat: Allow different tenants with the same account to register (#126)

--- application/application-standard/src/main/resources/db/h2/sql/v1.2.0/user.sql
+++ application/application-standard/src/main/resources/db/h2/sql/v1.2.0/user.sql
@@ -3,3 +3,9 @@
 --changeset loong:user_v1.2.0_20250224_155400
 ALTER TABLE `t_tenant`
     ADD COLUMN time_zone VARCHAR(255) not null DEFAULT 'GMT+08:00';
+
+--changeset loong:user_v1.2.0_20250321_095400
+alter table t_user
+drop constraint uk_email;
+
+alter table t_user add constraint uk_email unique(email, tenant_id);
