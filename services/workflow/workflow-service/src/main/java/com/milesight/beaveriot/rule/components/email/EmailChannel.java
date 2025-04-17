cd4bfc48d0c88c0f00cd91796c4a7055da2d51ab
Maglitch65 <majiajun@milesight.com>
2025-04-17 17:50:09 +0800

    feat: add SMTP credentials

--- services/workflow/workflow-service/src/main/java/com/milesight/beaveriot/rule/components/email/EmailChannel.java
+++ services/workflow/workflow-service/src/main/java/com/milesight/beaveriot/rule/components/email/EmailChannel.java
@@ -2,7 +2,7 @@ package com.milesight.beaveriot.rule.components.email;
 
 import java.util.List;
 
-public interface EmailChannel {
+public interface EmailChannel extends AutoCloseable {
 
     void send(String fromName, String fromAddress, List<String> to, String subject, String content);
 
