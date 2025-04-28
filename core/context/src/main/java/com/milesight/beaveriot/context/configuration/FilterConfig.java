ccdad6e09c9098e3a625af046a4665e8c904ab42
Maglitch65 <majiajun@milesight.com>
2025-04-28 11:33:58 +0800

    fix: handle snake case query parameter name

--- core/context/src/main/java/com/milesight/beaveriot/context/configuration/FilterConfig.java
+++ core/context/src/main/java/com/milesight/beaveriot/context/configuration/FilterConfig.java
@@ -2,6 +2,7 @@ package com.milesight.beaveriot.context.configuration;
 
 import com.milesight.beaveriot.authentication.facade.IAuthenticationFacade;
 import com.milesight.beaveriot.context.filter.HttpRequestFilter;
+import com.milesight.beaveriot.context.filter.QueryParameterNameConvertionFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -17,10 +18,18 @@ public class FilterConfig {
     private IAuthenticationFacade authenticationFacade;
 
     @Bean
-    public FilterRegistrationBean<HttpRequestFilter> customFilter() {
+    public FilterRegistrationBean<HttpRequestFilter> authenticationFilter() {
         FilterRegistrationBean<HttpRequestFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(new HttpRequestFilter(authenticationFacade));
         registrationBean.addUrlPatterns("/*");
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<QueryParameterNameConvertionFilter> queryParameterNameConvertionFilter() {
+        FilterRegistrationBean<QueryParameterNameConvertionFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new QueryParameterNameConvertionFilter());
+        registrationBean.addUrlPatterns("*");
         return registrationBean;
     }
 
---
+++ core/context/src/main/java/com/milesight/beaveriot/context/filter/QueryParameterNameConvertionFilter.java
@@ -0,0 +1,57 @@
+package com.milesight.beaveriot.context.filter;
+
+import com.google.common.base.CaseFormat;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.*;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class QueryParameterNameConvertionFilter extends OncePerRequestFilter {
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return request.getRequestURI().startsWith("/workflow-http-in");
+    }
+
+    @Override
+    @SneakyThrows
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) {
+        Map<String, String[]> params = new ConcurrentHashMap<>();
+
+        for (String paramName : request.getParameterMap().keySet()) {
+            params.put(paramName, request.getParameterValues(paramName));
+            String camelCaseParamName = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, paramName);
+            params.computeIfAbsent(camelCaseParamName, k -> request.getParameterValues(paramName));
+        }
+
+        filterChain.doFilter(new HttpServletRequestWrapper(request) {
+            @Override
+            public String getParameter(String name) {
+                return params.containsKey(name) ? params.get(name)[0] : null;
+            }
+
+            @Override
+            public Enumeration<String> getParameterNames() {
+                return Collections.enumeration(params.keySet());
+            }
+
+            @Override
+            public String[] getParameterValues(String name) {
+                return params.get(name);
+            }
+
+            @Override
+            public Map<String, String[]> getParameterMap() {
+                return params;
+            }
+        }, response);
+    }
+
+}
