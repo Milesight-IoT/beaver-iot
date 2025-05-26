8d2cb3225363098e2559aee23eeb5f0fe2bff4cb
Maglitch65 <majiajun@milesight.com>
2025-05-26 10:39:12 +0800

    perf: cache parsed SQL

--- services/permission/permission-service/src/main/java/com/milesight/beaveriot/permission/inspector/DataAspectStatementInspector.java
+++ services/permission/permission-service/src/main/java/com/milesight/beaveriot/permission/inspector/DataAspectStatementInspector.java
@@ -1,11 +1,14 @@
 package com.milesight.beaveriot.permission.inspector;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.milesight.beaveriot.permission.context.DataAspectContext;
 import lombok.*;
+import lombok.extern.slf4j.*;
 import net.sf.jsqlparser.expression.Expression;
-import net.sf.jsqlparser.expression.LongValue;
 import net.sf.jsqlparser.expression.Parenthesis;
 import net.sf.jsqlparser.expression.StringValue;
+import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
 import net.sf.jsqlparser.expression.operators.relational.EqualsTo;
 import net.sf.jsqlparser.expression.operators.relational.ExpressionList;
 import net.sf.jsqlparser.expression.operators.relational.InExpression;
@@ -20,143 +23,129 @@ import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
 import net.sf.jsqlparser.statement.update.Update;
 import org.hibernate.resource.jdbc.spi.StatementInspector;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
  * @author loong
  * @date 2024/12/5 15:12
  */
+@Slf4j
 public class DataAspectStatementInspector implements StatementInspector {
 
+    private static final String TENANT_ID_COLUMN_PLACEHOLDER = "${TENANT_ID_COL}";
+    private static final String TENANT_ID_VALUE_PLACEHOLDER = "${TENANT_ID_VALUE}";
+    private static final String DATA_ID_COLUMN_PLACEHOLDER = "${DATA_ID_COL}";
+
+    private static final Cache<String, SqlInfo> sqlTemplateCache = CacheBuilder.newBuilder()
+            .initialCapacity(1000)
+            .maximumSize(10000)
+            .concurrencyLevel(8)
+            .expireAfterAccess(7, TimeUnit.DAYS)
+            .build();
+
     @Override
+    @SneakyThrows
     public String inspect(String sql) {
-        String tenantSql = doTenantInspect(sql);
-        return doDataPermissionInspect(tenantSql);
+        SqlInfo cachedSql = sqlTemplateCache.getIfPresent(sql);
+        if (cachedSql == null) {
+            Statement statement = CCJSqlParserUtil.parse(sql);
+            TableInfo tableInfo = getTableInfo(statement);
+            String tableName = tableInfo.tableName;
+            doTenantInspect(statement);
+            doDataPermissionInspect(statement);
+            cachedSql = new SqlInfo(tableName, tableInfo.alias, statement.toString());
+            sqlTemplateCache.put(sql, cachedSql);
+        }
+
+        String tableName = cachedSql.tableName;
+        String tableAlias = cachedSql.tableAlias == null ? tableName : cachedSql.tableAlias;
+        String sqlTemplate = cachedSql.sqlTemplate;
+
+        DataAspectContext.TenantContext tenantContext = DataAspectContext.getTenantContext(tableName);
+        if (tenantContext != null && StringUtils.hasText(tenantContext.getTenantId())) {
+            String tenantId = tenantContext.getTenantId();
+            String columnName = tableAlias + "." + tenantContext.getTenantColumnName();
+            sqlTemplate = sqlTemplate.replace(TENANT_ID_COLUMN_PLACEHOLDER, columnName)
+                    .replace(TENANT_ID_VALUE_PLACEHOLDER, tenantId);
+        } else {
+            sqlTemplate = sqlTemplate.replace(TENANT_ID_COLUMN_PLACEHOLDER, "'1'")
+                    .replace(TENANT_ID_VALUE_PLACEHOLDER, "1");
+        }
+
+        DataAspectContext.DataPermissionContext dataPermissionContext = DataAspectContext.getDataPermissionContext(tableName);
+        if (dataPermissionContext != null && !CollectionUtils.isEmpty(dataPermissionContext.getDataIds())) {
+            String columnName = tableAlias + "." + dataPermissionContext.getDataColumnName();
+            String dataIdListString = dataPermissionContext.getDataIds()
+                    .stream()
+                    .map(String::valueOf)
+                    .collect(Collectors.joining(","));
+            sqlTemplate = sqlTemplate.replace(DATA_ID_COLUMN_PLACEHOLDER, columnName)
+                    .replace("IN ()", "IN (" + dataIdListString + ")");
+        } else {
+            sqlTemplate = sqlTemplate.replace(DATA_ID_COLUMN_PLACEHOLDER, "1")
+                    .replace("IN ()", "IN (1)");
+        }
+
+        return sqlTemplate;
     }
 
-    @SneakyThrows
-    private String doTenantInspect(String sql) {
-        Statement statement = CCJSqlParserUtil.parse(sql);
+    private void doTenantInspect(Statement statement) {
         if (statement instanceof Select selectStatement) {
             PlainSelect plainSelect = selectStatement.getPlainSelect();
-            addTenantCondition(plainSelect);
+            plainSelect.setWhere(wrapWhereExpressionWithTenantCondition(plainSelect.getWhere()));
         } else if (statement instanceof Update updateStatement) {
-            addTenantCondition(updateStatement);
+            updateStatement.setWhere(wrapWhereExpressionWithTenantCondition(updateStatement.getWhere()));
         } else if (statement instanceof Delete deleteStatement) {
-            addTenantCondition(deleteStatement);
+            deleteStatement.setWhere(wrapWhereExpressionWithTenantCondition(deleteStatement.getWhere()));
         } else if (statement instanceof Insert insertStatement) {
-            addTenantCondition(insertStatement);
+            addTenantValue(insertStatement);
         }
-        return statement.toString();
     }
 
-    @SneakyThrows
-    private String doDataPermissionInspect(String sql) {
-        Statement statement = CCJSqlParserUtil.parse(sql);
+    private void doDataPermissionInspect(Statement statement) {
         if (statement instanceof Select selectStatement) {
             PlainSelect plainSelect = selectStatement.getPlainSelect();
             addDataPermissionCondition(plainSelect);
         }
-        return statement.toString();
     }
 
-    private void addTenantCondition(PlainSelect plainSelect) {
-        String tableName = null;
-        String alias = null;
-        FromItem fromItem = plainSelect.getFromItem();
-        if (fromItem instanceof Table table) {
-            tableName = table.getName();
-            alias = table.getAlias() != null ? table.getAlias().getName() : tableName;
-        }
-        if (tableName == null || alias == null) {
-            return;
-        }
-        String tenantId = null;
-        String columnName = null;
-        if (DataAspectContext.isTenantEnabled(tableName)) {
-            DataAspectContext.TenantContext tenantContext = DataAspectContext.getTenantContext(tableName);
-            if (tenantContext != null) {
-                tenantId = tenantContext.getTenantId();
-                columnName = tenantContext.getTenantColumnName();
+    private TableInfo getTableInfo(Statement statement) {
+        if (statement instanceof Select selectStatement) {
+            FromItem fromItem = selectStatement.getPlainSelect().getFromItem();
+            if (fromItem instanceof Table table) {
+                return getTableInfo(table);
             }
+        } else if (statement instanceof Update updateStatement) {
+            return getTableInfo(updateStatement.getTable());
+        } else if (statement instanceof Delete deleteStatement) {
+            return getTableInfo(deleteStatement.getTable());
+        } else if (statement instanceof Insert insertStatement) {
+            return getTableInfo(insertStatement.getTable());
         }
-        if (tenantId == null || columnName == null) {
-            return;
-        }
-        Column column = new Column(alias + "." + columnName);
-        Expression tenantExpression = new EqualsTo(column, new StringValue(tenantId));
+        log.error("Unsupported SQL statement: {}", statement);
+        throw new IllegalArgumentException("Unsupported SQL statement");
+    }
 
-        if (plainSelect.getWhere() == null) {
-            plainSelect.setWhere(tenantExpression);
+    private TableInfo getTableInfo(Table table) {
+        return new TableInfo(table.getName(), table.getAlias() != null ? table.getAlias().getName() : table.getName());
+    }
+
+    private Expression wrapWhereExpressionWithTenantCondition(Expression originalWhere) {
+        Column column = new Column(TENANT_ID_COLUMN_PLACEHOLDER);
+        Expression tenantExpression = new EqualsTo(column, new StringValue(TENANT_ID_VALUE_PLACEHOLDER));
+        if (originalWhere == null) {
+            return tenantExpression;
         } else {
-            Parenthesis originalWhere = new Parenthesis(plainSelect.getWhere());
-            plainSelect.setWhere(new net.sf.jsqlparser.expression.operators.conditional.AndExpression(
-                    originalWhere, tenantExpression));
+            return new AndExpression(new Parenthesis(originalWhere), tenantExpression);
         }
     }
 
-    private void addTenantCondition(Update updateStatement) {
-        Table table = updateStatement.getTable();
-        String tableName = table.getName();
-        String alias = table.getAlias() != null ? table.getAlias().getName() : tableName;
-
-        String tenantId = null;
-        String columnName = null;
-        if (DataAspectContext.isTenantEnabled(tableName)) {
-            DataAspectContext.TenantContext tenantContext = DataAspectContext.getTenantContext(tableName);
-            if (tenantContext != null) {
-                tenantId = tenantContext.getTenantId();
-                columnName = tenantContext.getTenantColumnName();
-            }
-        }
-        if (tenantId == null || columnName == null) {
-            return;
-        }
-        Column column = new Column(alias + "." + columnName);
-        Expression tenantExpression = new EqualsTo(column, new StringValue(tenantId));
-
-        if (updateStatement.getWhere() == null) {
-            updateStatement.setWhere(tenantExpression);
-        } else {
-            Parenthesis originalWhere = new Parenthesis(updateStatement.getWhere());
-            updateStatement.setWhere(new net.sf.jsqlparser.expression.operators.conditional.AndExpression(
-                    originalWhere, tenantExpression));
-        }
-    }
-
-    private void addTenantCondition(Delete deleteStatement) {
-        Table table = deleteStatement.getTable();
-        String tableName = table.getName();
-        String alias = table.getAlias() != null ? table.getAlias().getName() : tableName;
-
-        String tenantId = null;
-        String columnName = null;
-        if (DataAspectContext.isTenantEnabled(tableName)) {
-            DataAspectContext.TenantContext tenantContext = DataAspectContext.getTenantContext(tableName);
-            if (tenantContext != null) {
-                tenantId = tenantContext.getTenantId();
-                columnName = tenantContext.getTenantColumnName();
-            }
-        }
-        if (tenantId == null || columnName == null) {
-            return;
-        }
-        Column column = new Column(alias + "." + columnName);
-        Expression tenantExpression = new EqualsTo(column, new StringValue(tenantId));
-
-        if (deleteStatement.getWhere() == null) {
-            deleteStatement.setWhere(tenantExpression);
-        } else {
-            Parenthesis originalWhere = new Parenthesis(deleteStatement.getWhere());
-            deleteStatement.setWhere(new net.sf.jsqlparser.expression.operators.conditional.AndExpression(
-                    originalWhere, tenantExpression));
-        }
-    }
-
-    private void addTenantCondition(Insert insertStatement) {
+    private void addTenantValue(Insert insertStatement) {
         List<Column> columns = insertStatement.getColumns();
         String tableName = insertStatement.getTable().getName();
         ExpressionList<?> itemList = insertStatement.getValues().getExpressions();
@@ -188,7 +177,7 @@ public class DataAspectStatementInspector implements StatementInspector {
             for (ExpressionList expressions : (ExpressionList<ExpressionList>) itemList) {
 
                 if (!tenantIdPresent) {
-                    expressions.add(new StringValue(tenantId));
+                    expressions.add(new StringValue(TENANT_ID_VALUE_PLACEHOLDER));
                 }
 
                 // Ensure the columns and expressions lists are of the same size for each row
@@ -200,7 +189,7 @@ public class DataAspectStatementInspector implements StatementInspector {
             List<Expression> expressions = (ExpressionList<Expression>) itemList;
 
             if (!tenantIdPresent) {
-                expressions.add(new StringValue(tenantId));
+                expressions.add(new StringValue(TENANT_ID_VALUE_PLACEHOLDER));
             }
 
             // Ensure the columns and expressions lists are of the same size
@@ -221,31 +210,22 @@ public class DataAspectStatementInspector implements StatementInspector {
         if (tableName == null || alias == null) {
             return;
         }
-        List<Long> dataIds = new ArrayList<>();
-        String columnName = null;
-        if (DataAspectContext.isDataPermissionEnabled(tableName)) {
-            DataAspectContext.DataPermissionContext dataPermissionContext = DataAspectContext.getDataPermissionContext(tableName);
-            if (dataPermissionContext != null) {
-                dataIds = dataPermissionContext.getDataIds();
-                columnName = dataPermissionContext.getDataColumnName();
-            }
-        }
-        if (dataIds.isEmpty() || columnName == null) {
-            return;
-        }
-        Column column = new Column(alias + "." + columnName);
-        ExpressionList<Expression> expressionList = new ExpressionList<>(dataIds.stream()
-                .map(LongValue::new)
-                .collect(Collectors.toList()));
-        Expression expression = new InExpression(column, new Parenthesis(expressionList));
+        Column column = new Column(DATA_ID_COLUMN_PLACEHOLDER);
+        // Use 'IN ()' as a placeholder
+        Expression expression = new InExpression(column, new Parenthesis(new ExpressionList<>()));
 
         if (plainSelect.getWhere() == null) {
             plainSelect.setWhere(expression);
         } else {
             Parenthesis originalWhere = new Parenthesis(plainSelect.getWhere());
-            plainSelect.setWhere(new net.sf.jsqlparser.expression.operators.conditional.AndExpression(
-                    originalWhere, expression));
+            plainSelect.setWhere(new AndExpression(originalWhere, expression));
         }
+    }
+
+    private record SqlInfo(String tableName, String tableAlias, String sqlTemplate) {
+    }
+
+    private record TableInfo(String tableName, String alias) {
     }
 
 }
