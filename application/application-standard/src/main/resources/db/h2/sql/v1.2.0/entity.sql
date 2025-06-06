5d6acf789fee2de788c5129219f995c4f03d4a9f
pandalxb <fzuxx@qq.com>
2025-06-06 15:12:32 +0800

    feat: add deleteByKey function for EntityServiceProvider (#223)
    * Feat: add deleteByKey function for EntityServiceProvider
    
    * Fix: add index to t_entity table

--- application/application-standard/src/main/resources/db/h2/sql/v1.2.0/entity.sql
+++ application/application-standard/src/main/resources/db/h2/sql/v1.2.0/entity.sql
@@ -8,3 +8,6 @@ EXECUTE IMMEDIATE 'ALTER TABLE t_entity DROP CONSTRAINT ' ||
     LIMIT 1);
 
 alter table t_entity add constraint uk_entity_key unique("key", tenant_id);
+
+--changeset pandalxb:entity_v1.2.0_20250606_145500
+CREATE INDEX idx_entity_parent ON `t_entity` (parent);
