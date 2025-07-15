a9a9b940803647078103581fe4e0cd688f81815a
pandalxb <fzuxx@qq.com>
2025-07-15 13:05:46 +0800

    Modify: (#271)
    1. Allow string value when entity type is double or long
    2. Allow empty string when entity is optional

--- core/base/src/main/java/com/milesight/beaveriot/base/utils/ValidationUtils.java
+++ core/base/src/main/java/com/milesight/beaveriot/base/utils/ValidationUtils.java
@@ -5,9 +5,11 @@ package com.milesight.beaveriot.base.utils;
  * create: 2025/7/14 8:53
  **/
 public class ValidationUtils {
-    private static final String REGEX_HEX = "^[0-9a-fA-F]+$";
+    private static final String REGEX_HEX = "^[0-9a-fA-F]*$";
     private static final String REGEX_URL = "^http(s)?:\\/\\/[-\\w]+((\\.[-\\w]+)+)?((\\/[-\\w]+)+)?(\\.[a-zA-Z]+)?(\\/)?((\\?[a-zA-Z]+[0-9a-zA-Z_]*\\=[-\\w]+)((&[a-zA-Z]+[0-9a-zA-Z_]*\\=[-\\w]+)+)?)?$";
     private static final String REGEX_IMAGE_BASE64 = "^data:image\\/(png|jpe?g|gif|webp);base64,[A-Za-z0-9+\\/=]+$";
+    private static final String REGEX_NUMBER = "^-?\\d+(\\.\\d+)?$";
+    private static final String REGEX_INTEGER = "^-?\\d+$";
 
     public static boolean isHex(String text) {
         return matches(text, REGEX_HEX);
@@ -19,6 +21,14 @@ public class ValidationUtils {
 
     public static boolean isImageBase64(String text) {
         return matches(text, REGEX_IMAGE_BASE64);
+    }
+
+    public static boolean isNumber(String text) {
+        return matches(text, REGEX_NUMBER);
+    }
+
+    public static boolean isInteger(String text) {
+        return matches(text, REGEX_INTEGER);
     }
 
     public static boolean matches(String text, String regex) {
