b34b61df01069814f4808da8fef7a1b8f0078fa0
pandalxb <fzuxx@qq.com>
2025-07-17 13:25:56 +0800

    feat: enable setting custom expiration minutes for resources (#281)
    * 1. Supports custom device id and device name fields in input definition
    2. Optimize device template schema
    
    * Feat: Enable setting custom expiration minutes for resources

--- services/resource-manager/resource-service/src/main/java/com/milesight/beaveriot/resource/manager/constants/ResourceManagerConstants.java
+++ services/resource-manager/resource-service/src/main/java/com/milesight/beaveriot/resource/manager/constants/ResourceManagerConstants.java
@@ -12,6 +12,8 @@ import java.time.temporal.ChronoUnit;
 public class ResourceManagerConstants {
     private ResourceManagerConstants() {}
 
+    public static final int MAX_TEMP_RESOURCE_LIVE_MINUTES = 360;
+
     public static final int TEMP_RESOURCE_LIVE_MINUTES = 15;
 
     public static final Duration CLEAR_TEMP_RESOURCE_INTERVAL = Duration.of(1, ChronoUnit.MINUTES);
