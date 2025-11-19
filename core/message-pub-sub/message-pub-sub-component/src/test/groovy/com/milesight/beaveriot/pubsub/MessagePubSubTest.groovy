c0aacab9c691da2024e6887db18c2f2028799837
Maglitch65 <majiajun@milesight.com>
2025-11-19 18:03:11 +0800

    test: fix broken unit tests

--- core/message-pub-sub/message-pub-sub-component/src/test/groovy/com/milesight/beaveriot/pubsub/MessagePubSubTest.groovy
+++ core/message-pub-sub/message-pub-sub-component/src/test/groovy/com/milesight/beaveriot/pubsub/MessagePubSubTest.groovy
@@ -1,13 +1,17 @@
 package com.milesight.beaveriot.pubsub
 
 import com.milesight.beaveriot.TestApplication
+import com.milesight.beaveriot.authentication.facade.IAuthenticationFacade
 import com.milesight.beaveriot.pubsub.api.annotation.MessageListener
 import com.milesight.beaveriot.pubsub.api.message.LocalUnicastMessage
 import com.milesight.beaveriot.pubsub.api.message.RemoteBroadcastMessage
+import com.milesight.beaveriot.rule.RuleEngineExecutor
+import org.spockframework.spring.SpringBean
 import org.spockframework.spring.SpringSpy
 import org.spockframework.spring.UnwrapAopProxy
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.core.task.TaskExecutor
 import org.springframework.stereotype.Component
 import spock.lang.Specification
 
@@ -20,6 +24,18 @@ class MessagePubSubTest extends Specification {
     @SpringSpy
     @UnwrapAopProxy
     TestListener testListener
+
+    @SpringBean(name = "mqtt")
+    TaskExecutor mqtt = { task -> task.run() }
+
+    @SpringBean(name = "mqtt-subscriber")
+    TaskExecutor mqttSubscriber = { task -> task.run() }
+
+    @SpringBean
+    IAuthenticationFacade authenticationFacade = Mock()
+
+    @SpringBean
+    RuleEngineExecutor ruleEngineExecutor = Mock()
 
     def "test publish"() {
         given:
