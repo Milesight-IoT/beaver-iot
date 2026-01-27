b26a0ad72ac7bbda695346d4a97e7b5d36989595
pandalxb <fzuxx@qq.com>
2026-01-27 17:24:53 +0800

    fix: Error principal name issue

--- services/authentication/authentication-service/src/main/java/com/milesight/beaveriot/authentication/provider/CustomOAuth2RefreshTokenAuthenticationProvider.java
+++ services/authentication/authentication-service/src/main/java/com/milesight/beaveriot/authentication/provider/CustomOAuth2RefreshTokenAuthenticationProvider.java
@@ -227,7 +227,7 @@ public class CustomOAuth2RefreshTokenAuthenticationProvider implements Authentic
         // @formatter:off
         DefaultOAuth2TokenContext.Builder tokenContextBuilder = DefaultOAuth2TokenContext.builder()
                 .registeredClient(registeredClient)
-                .principal(new UsernamePasswordAuthenticationToken(refreshTokenAuthentication.getName(), null))
+                .principal(new UsernamePasswordAuthenticationToken(authorization.getPrincipalName(), null))
                 .authorizationServerContext(AuthorizationServerContextHolder.getContext())
                 .authorization(authorization)
                 .authorizedScopes(scopes)
